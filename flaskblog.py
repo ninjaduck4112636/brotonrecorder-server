16dbedab1eb022965ba6cef99619aed4cccb4cf5
ninjaduck4112636 <ninjaduck4112636@gmail.com>
2021-03-09 02:12:43 -0800

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import requests
+import datetime
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def sensor():
+	date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	response = requests.get('https://brotonrecorder.gq').status_code
+	res = {date_time:response}
+	print(res)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor,'interval',minutes=0.2)
+sched.start()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return '''
+		<!DOCTYPE html>
+<html>
+<head>
+	<title>brotonrecorder-server</title>
+</head>
+<body>
+	website: brotonrecorder.gq
+</body>
+</html>
+	'''
+
+
+if __name__ == "__main__":
+    app.run()	
